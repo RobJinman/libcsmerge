8e237f04aad13c8fe0e4c6de7365f105bff93bdd
Rob Jinman <jinmanr@gmail.com>
2019-05-18 20:09:07 +0100

    Fix setup.py to work on OS X Mojave

--- pycsmerge/setup.py
+++ pycsmerge/setup.py
@@ -22,29 +22,35 @@ module1 = Extension('_pycsmerge',
                     ],
                     libraries=[
                         'csmerge',
-                        'boost_python3',
+                        'boost_python37',
                         'mpfr',
                         'gmp',
                         'CGAL',
                         'CGAL_Core',
-                        'boost_thread',
-                        'boost_system',
+                        'boost_thread-mt',
+                        'boost_system-mt',
                         'pthread'
                     ],
                     extra_compile_args=[
+                        '-stdlib=libc++',
+                        '-mmacosx-version-min=10.11',
                         '-std=c++11',
                         '-O3',
                         '-frounding-math',
                         '-Wall',
                         '-fPIC',
                         '-Wl,-soname,_pycsmerge.so'
+                    ],
+                    extra_link_args=[
+                        '-stdlib=libc++',
+                        '-mmacosx-version-min=10.11'
                     ])
 
 setup(
     name='Pycsmerge',
     version='1.0.0',
     author='Rob Jinman',
-    author_email='info@recursiveloop.org',
+    author_email='jinmanr@gmail.com',
     packages=['pycsmerge'],
     license='Proprietry',
     install_requires=[''],
